316915e4b6ed840231c6bedad282edab870017d5
Muhammad Afdhaluddin <afdhaluddin.zainudin@uemsunrise.uemnet.com>
2019-12-26 10:44:46 +0800

    Updates

---
+++ precache-manifest.7b571fdf138b538991d8d962e8e9ffce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebd32a32b2e6ac890e7cb61d1e4d650",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "65e2e56920469cfc2c69",
+    "url": "/monsters-rolodex/static/css/main.825b11a0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65e2e56920469cfc2c69",
+    "url": "/monsters-rolodex/static/js/main.7d9e5496.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
